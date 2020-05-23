edfc98dda3e9dee08676a72a0fde539c3734448c
vamsi kasyap <55094394+vamsikasyapsalva@users.noreply.github.com>
2020-05-23 17:35:50 +0530

    Create ananum.py

---
+++ ananum.py
@@ -0,0 +1,17 @@
+s1 = input()
+s2 = input()
+a = []
+b = []
+d = []
+for i in range(len(s1)):
+    for j in range(i+1,len(s1)+1):
+        a.append(s1[i:j])
+#print(a)
+for i in a:
+    if len(i) == len(s2) and i[0] in s2 and i[len(i)-1] in s2:
+        b.append(i)
+#print(b)
+print('no of anagrams:',len(b))
+for i in b:
+    d.append(s1.find(i))
+print(d)
